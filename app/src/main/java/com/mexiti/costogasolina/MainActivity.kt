06ecba5f060e9e3f2d9962430f7b518a80574996
Armando <jarodriguezvera@gmail.com>
2024-02-11 15:13:48 -0600

    Estructura inicial de la aplicación

--- app/src/main/java/com/mexiti/costogasolina/MainActivity.kt
+++ app/src/main/java/com/mexiti/costogasolina/MainActivity.kt
@@ -3,12 +3,23 @@ package com.mexiti.costogasolina
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import com.mexiti.costogasolina.ui.theme.CostoGasolinaTheme
 
@@ -30,11 +41,61 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun CostGasLayout(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name",
-        modifier = modifier
+fun CostGasLayout(name: String) {
+    Column {
+        Text(
+            text = stringResource(R.string.calcular_monto),
+
+            )
+       EditNumberField(
+           label = R.string.ingresa_gasolina,
+           leadingIcon = R.drawable.money_gas ,
+           keyboardsOptions = KeyboardOptions.Default.copy(
+               keyboardType = KeyboardType.Number,
+               imeAction = ImeAction.Next
+           ),
+           value = "10.0",
+           onValueChanged = {}
+       )
+        TextField(
+            value = stringResource(R.string.litros),
+            onValueChange = {}
+        )
+        TextField(
+            value = stringResource(R.string.propina) ,
+            onValueChange = {}
+        )
+        Switch(
+            checked = false,
+            onCheckedChange =  {}
+        )
+        Text(
+            text = "Total:  $"
+        )
+
+    }
+
+}
+
+@Composable
+fun EditNumberField(
+    @StringRes label: Int,
+    @DrawableRes leadingIcon: Int,
+    keyboardsOptions:KeyboardOptions,
+    value: String,
+    onValueChanged: (String) -> Unit,
+    modifier: Modifier = Modifier
+){
+    TextField(
+        label = { Text(text = stringResource(id = label))  },
+        value = value,
+        singleLine = true,
+        leadingIcon = { Icon(painter = painterResource(id = leadingIcon) , contentDescription = null) },
+        keyboardOptions = keyboardsOptions,
+        modifier = modifier,
+        onValueChange = onValueChanged
     )
+
 }
 
 @Preview(showBackground = true)
